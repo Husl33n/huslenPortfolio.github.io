9a1f0d814ad31d72563589f8542f727c8b1faa07
husl33n <bhuslen8863@gmail.com>
2023-05-01 00:25:10 +0800

    skills small size

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -6,35 +6,47 @@ const Skill = () => {
     <div className="mb-20 mx-6 flex flex-col items-center justify-center md:w-7/12">
       <Title>Мэдлэг</Title>
       <div className="flex flex-1 flex-row items-center justify-center flex-wrap gap-4">
-        <div className=" bg-stone-200 shadow-2xl p-5 rounded-full">
-          <img className=" h-20" src="assets/skills/html.png" alt="" />
+        <div className=" bg-stone-200 shadow-2xl p-4 md:p-5 rounded-full">
+          <img className="h-12 md:h-20" src="assets/skills/html.png" alt="" />
         </div>
-        <div className=" bg-stone-200 shadow-2xl p-5 rounded-full">
-          <img className=" h-20" src="assets/skills/css.png" alt="" />
+        <div className=" bg-stone-200 shadow-2xl p-4 md:p-5 rounded-full">
+          <img className="h-12 md:h-20" src="assets/skills/css.png" alt="" />
         </div>
-        <div className=" bg-stone-200 shadow-2xl p-5 rounded-full">
-          <img className=" h-20" src="assets/skills/tailwindcss.png" alt="" />
+        <div className=" bg-stone-200 shadow-2xl p-4 md:p-5 rounded-full">
+          <img
+            className="h-12 md:h-20"
+            src="assets/skills/tailwindcss.png"
+            alt=""
+          />
         </div>
-        <div className=" bg-stone-200 shadow-2xl p-5 rounded-full">
-          <img className=" h-20" src="assets/skills/javascript.png" alt="" />
+        <div className=" bg-stone-200 shadow-2xl p-4 md:p-5 rounded-full">
+          <img
+            className="h-12 md:h-20"
+            src="assets/skills/javascript.png"
+            alt=""
+          />
         </div>
-        <div className=" bg-stone-200 shadow-2xl p-5 rounded-full">
-          <img className=" h-20" src="assets/skills/reactjs.png" alt="" />
+        <div className=" bg-stone-200 shadow-2xl p-4 md:p-5 rounded-full">
+          <img
+            className="h-12 md:h-20"
+            src="assets/skills/reactjs.png"
+            alt=""
+          />
         </div>
-        <div className=" bg-stone-200 shadow-2xl p-5 rounded-full">
-          <img className=" h-20" src="assets/skills/java.png" alt="" />
+        <div className=" bg-stone-200 shadow-2xl p-4 md:p-5 rounded-full">
+          <img className="h-12 md:h-20" src="assets/skills/java.png" alt="" />
         </div>
-        <div className=" bg-stone-200 shadow-2xl p-5 rounded-full">
-          <img className=" h-20" src="assets/skills/csharp.png" alt="" />
+        <div className=" bg-stone-200 shadow-2xl p-4 md:p-5 rounded-full">
+          <img className="h-12 md:h-20" src="assets/skills/csharp.png" alt="" />
         </div>
-        <div className=" bg-stone-200 shadow-2xl p-5 rounded-full">
-          <img className=" h-20" src="assets/skills/mysql.png" alt="" />
+        <div className=" bg-stone-200 shadow-2xl p-4 md:p-5 rounded-full">
+          <img className="h-12 md:h-20" src="assets/skills/mysql.png" alt="" />
         </div>
-        <div className=" bg-stone-200 shadow-2xl p-5 rounded-full">
-          <img className=" h-20" src="assets/skills/ps.png" alt="" />
+        <div className=" bg-stone-200 shadow-2xl p-4 md:p-5 rounded-full">
+          <img className="h-12 md:h-20" src="assets/skills/ps.png" alt="" />
         </div>
-        <div className=" bg-stone-200 shadow-2xl p-5 rounded-full">
-          <img className=" h-20" src="assets/skills/github.png" alt="" />
+        <div className=" bg-stone-200 shadow-2xl p-4 md:p-5 rounded-full">
+          <img className="h-12 md:h-20" src="assets/skills/github.png" alt="" />
         </div>
       </div>
     </div>
